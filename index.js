bfccde864079d60addafd020461f63e7dd917902
Melissa <melissa.wanjiku@student.moringaschool.com>
2023-12-23 23:13:48 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  });
+  
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+
+  document.addEventListener('DOMContentLoaded', function() {
+   
+    const textParagraph = document.getElementById('text');
+    textParagraph.textContent = 'This is really cool!';
+  });
